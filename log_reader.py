4a2fe5983626eed92f01523e25b5db0d43048bf6
Andy Davila <andy.davila@gmail.com>
2021-05-09 17:26:08 -0400

    Minor edits to reader

--- log_reader.py
+++ log_reader.py
@@ -21,8 +21,10 @@ class LogReader(threading.Thread):
             log_lines = self.tail_file(log_file)
             for log_line in log_lines:
                 parsed_log_line = self.parse_log_line(log_line)
-                self.alert_queue.append(parsed_log_line['time'])
-                self.stats_queue.append(parsed_log_line)
+                # Only add if the parsed line has been parsed correctly
+                if parsed_log_line:
+                    self.alert_queue.append(parsed_log_line['time'])
+                    self.stats_queue.append(parsed_log_line)
 
     # Tailing file implementation is from a presentation
     # discussing different tools leveraging Python generators
@@ -37,7 +39,7 @@ class LogReader(threading.Thread):
             yield line
 
     def parse_log_line(self, log_line):
-        # regex adopted from...
+        # Regex adopted from...
         # https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch07s12.html
         regex = re.compile(
             r'^(?P<client>\S+),(?P<skip>\S+),(?P<userid>\S+),(?P<time>.+),'
@@ -51,11 +53,12 @@ class LogReader(threading.Thread):
 
         matched_dict = matched.groupdict()
 
+        # In case there any log lines with malformed lines, return none
         try:
             matched_dict['time'] = int(matched['time'])
             matched_dict['size'] = int(matched['size'])
             matched_dict['section'] = matched_dict['section'].split('/')[1]
-        except ValueError:
-            raise Exception
+        except (ValueError, IndexError):
+            return None
 
         return matched_dict
