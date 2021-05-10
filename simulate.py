3ca32b45fb6e053e638a610e06ebaa054623328f
Andy Davila <andy.davila@gmail.com>
2021-05-10 15:58:40 -0400

    Update simulator for clean exit and parameters

--- simulate.py
+++ simulate.py
@@ -1,15 +1,29 @@
 from random import random
+import argparse
 import time
 
 
 def simulate(input_file, output_file):
-    with open(input_file, "r") as log_lines:
-        with open(output_file, 'w', buffering=1) as log_file:
-            for log_line in log_lines:
-                if log_line:
-                    log_file.write(log_line)
-                time.sleep(random())
+    try:
+        with open(input_file, "r") as log_lines:
+            with open(output_file, 'w', buffering=1) as log_file:
+                for log_line in log_lines:
+                    if log_line:
+                        log_file.write(log_line)
+                    time.sleep(random())
+    except KeyboardInterrupt:
+        print("\nSimulation interrupted")
+        with open(output_file, 'w') as log_file:
+            log_file.truncate(0)
 
 
 if __name__ == '__main__':
-    log_lines = simulate('sample_csv.txt', 'log-file.log')
+    parser = argparse.ArgumentParser(description="Simulate logging")
+    parser.version = '1.0'
+
+    parser.add_argument('DATA_FILE_PATH', type=str, help="path to data file")
+    parser.add_argument('LOG_FILE', type=str, help="path to log file")
+
+    args = parser.parse_args()
+
+    simulate(args.DATA_FILE_PATH, args.LOG_FILE)
